76b016af63eecf1b68d0b7edf74756922489a3cb
Vsevolod Novikov <nnseva@gmail.com>
2020-04-08 18:37:32 +0300

    Fixed validation for the django_extensions JSONField replacement

--- jsoneditor/fields/django_extensions_jsonfield.py
+++ jsoneditor/fields/django_extensions_jsonfield.py
@@ -1,14 +1,17 @@
 from django_extensions.db.fields.json import JSONField as _JSONField
 from django import forms
+from django.core.exceptions import ValidationError
 
 from jsoneditor.forms import JSONEditor
+
+import json
+import six
 
 class JSONFormField(forms.CharField):
     widget = JSONEditor
     def __init__(self,*av,**kw):
         kw['widget'] = self.widget # force avoiding widget override
         super(JSONFormField,self).__init__(*av,**kw)
-
 
 class JSONField(_JSONField):
     def formfield(self, **kwargs):
@@ -17,3 +20,11 @@ class JSONField(_JSONField):
         }
         defaults.update(kwargs)
         return super(JSONField, self).formfield(**defaults)
+
+    def to_python(self, value):
+        if isinstance(value, six.string_types):
+            try:
+                json.loads(value)
+            except json.decoder.JSONDecodeError as ex:
+                raise ValidationError(ex)
+        return super(JSONField, self).to_python(value)
